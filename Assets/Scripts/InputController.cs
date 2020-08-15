6cbca5d2260da1e7f2d1ac6b683bea7c740c1be2
Furkan Bedir <furkanbdr99@hotmail.com>
2020-08-15 18:51:44 +0300

    basic scripts created

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject player;
+    public GameObject spawnPos;
+
+  
+         
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("test");
+            
+            
+            Instantiate(ball, spawnPos.transform.position, spawnPos.transform.rotation);
+        }
+    }
+}
+
+
+
