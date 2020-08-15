12f75d93c4d7907d70aa5f27897ef7ddeee12e97
furkiti <furkanbdr99@hotmail.com>
2020-08-16 00:34:37 +0300

    game controller added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,36 +5,17 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-   
-    public float spawnRate;
-    private float nextSpawn;
-
-    public GameObject targetSpawnPos;
-    public GameObject targets;
-    public GameObject targetParents;
-
-    public Text displayScore;
-    public static int gameScore;
+  
+    //public Text displayScore;
+    //public static int gameScore;
 
 
      void Update()
     {
         
-            if(Time.time > nextSpawn)
-            {
-                float randomdir = Random.Range(-2.4f, 2.4f);
-                float sizeNum = Random.Range(0.7f, 1.3f);
+            
 
-                nextSpawn = Time.time + spawnRate;
-
-                GameObject target = Instantiate(targets, new Vector2(targetSpawnPos.transform.position.x + randomdir,targetSpawnPos.transform.position.y),targetSpawnPos.transform.rotation);
-
-                target.transform.localScale = new Vector3(sizeNum, sizeNum, 0f);
-
-                target.transform.SetParent(targetParents.transform, true);
-            }
-
-            displayScore.text = gameScore.ToString();
+            //displayScore.text = gameScore.ToString();
         }   
     
 }
---
+++ Assets/Scripts/TargetGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TargetGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetprefab;
+    [SerializeField]
+    private GameObject targetSpawnPos;
+
+    private ObjectPool targetPool;
+    private int poolSize = 1;
+    private float[] posArray = {-2,-0.69f,0.62f,1.95f};
+
+    void Start()
+    {
+        targetPool = new ObjectPool(targetprefab);
+        targetPool.fillpool(poolSize);
+
+        StartCoroutine(createobj());
+
+    }
+
+    IEnumerator createobj()
+    {
+        while (true)
+        {
+            var wait = new WaitForSeconds(1f);
+
+            Vector3 pos = setpos();
+
+            GameObject tempobj = targetPool.popfrompool();
+            tempobj.transform.position = pos;
+
+            yield return wait;
+        }
+        
+    }
+
+    private Vector3 setpos()
+    {
+        int indexposx = Random.Range(0, posArray.Length);
+        
+       return new Vector3(posArray[indexposx],targetSpawnPos.transform.position.y,targetSpawnPos.transform.position.z);
+
+    }
+}
